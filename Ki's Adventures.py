781eca677c8ce3a920ed9e8f74d998e84ea219c7
Mrfisher199 <max.latysh@gmail.com>
2017-04-30 18:51:56 -0600

    Update Ki's Adventures.py

--- Ki's Adventures.py
+++ Ki's Adventures.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 #coding: latin-1
 #KAd or Ki's Adventures is a mario like game...
-#Made By
+#Made By Mrfisher199
 import curses, time, random
 from random import randint as ran
-s = curses.initscr()
+s = curses.initscr() # line 7 to line 26 is curses initiation.
 curses.initscr()
 curses.start_color()
 curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -15,7 +15,7 @@ curses.init_pair(5, 9, curses.COLOR_BLACK)
 curses.init_pair(6, 227, curses.COLOR_BLACK)
 curses.init_pair(7, 209, curses.COLOR_BLACK)
 curses.init_pair(8, 130, curses.COLOR_BLACK)
-print '\x1b[8;24;80t'
+print '\x1b[8;24;80t' #Changes size of screen.
 window = curses.newwin(24, 80, 0, 0)
 window.timeout(50)
 window.keypad(1)
@@ -24,69 +24,56 @@ curses.curs_set(0)
 window.border(0)
 print '\x1b[8;24;80t'
 dims = window.getmaxyx()
-def game():
-	dims=[24,80]
-	ls=[]
-	fly=1
+def game(): #game
+	dims=[24,80] #size of world
+	ls=[] #This is used for the bullets...
+	fly=1 #line 30 to line 31 are the jumping variables.
 	flys=1
 	c=0
-	cs=0
-	x=21;y=2
-	for i in range(20):
+	cs=0 # count
+	x=21;y=2 # location
+	for i in range(20): # Generating the base for the variable ls (line 29)
 		ls.append(([' ']*78))
-	lev=20
+	lev=20 # Level Variables
 	levs={1:11, 2:7, 3:6, 4:6, 5:6, 6:5, 7:5, 8:5, 9:4, 10:4, 11:4, 12:3, 13:3, 14:3, 15:2, 16:2, 17:2, 18:1, 19:1, 20:1}
-	pause=False
-	al=ran(4, 66)
-	als=ran(4, 66)
-	lss=[]
-	for i in range(3):
-		lss.append([' ']*78)
-	for i in range(13):
-		lss[2][als+i]='#'
-	for i in range(5):
-		lss[1][als+3+i]='#'
-	lss[0][als+6]='#'
-	css=0
+	pause=False # pause variable
+	al=ran(4, 66) #location of hill 1
+	als=ran(4, 66)	#location fo hill 2
 	health=5
 	while 1:
-		window.erase()
-		window.border(0)
-		window.addstr(1,1,''.join(['-']*78));window.addstr(22,1,''.join(['#']*78))
-		xs=2
-		for i in ls:
+		window.erase() # Clearing of the screen
+		window.border(0) # The border
+		window.addstr(1,1,''.join(['-']*78));window.addstr(22,1,''.join(['#']*78)) # Decoration...
+		xs=2 
+		for i in ls: # adding ls to the screen
 			window.addstr(xs, 1, ''.join(i))
 			xs+=1
-		window.addstr(0, 35, 'Health: '+str(health))
-		window.addstr(21, al,'#############');window.addstr(20,al+3,'#######');window.addstr(19,al+6,'#')
+		window.addstr(0, 35, 'Health: '+str(health)) #health
+		window.addstr(21, al,'#############');window.addstr(20,al+3,'#######');window.addstr(19,al+6,'#') # hills
 		window.addstr(21, als,'#############');window.addstr(20,als+3,'#######');window.addstr(19,als+6,'#')
 		if pause == True:
-			window.addstr(12, 23, 'Press Enter to Unpause or Press "q" to exit.')
-		window.addstr(x-1, y-1, '\\', curses.color_pair(1));window.addstr(x-1, y, '|', curses.color_pair(3));window.addstr(x-1, y+1, '/', curses.color_pair(8))
-		window.addstr(x, y-1, '|#|', curses.color_pair(7))
-		q=window.getch()
-		if q == ord('\n'):
+			window.addstr(12, 23, 'Press Enter to Unpause or Press "q" to exit.') # Display of option in this mode
+		window.addstr(x-1, y-1, '\\', curses.color_pair(1));window.addstr(x-1, y, '|', curses.color_pair(3));window.addstr(x-1, y+1, '/', curses.color_pair(8)) #display of character
+		window.addstr(x, y-1, '|#|', curses.color_pair(7)) # dicplay of character
+		q=window.getch() # checking for input from the user
+		if q == ord('\n'): #Pause
 			if pause == False:
 				pause=True
 			elif pause==True:
 				pause=False
 		if pause == False:
-			if y == 77:
+			if y == 77: # If new level
 				x=21;y=2
 				al=ran(4, 66)
 				als=random.randint(4, 66)
-				lss=[]
-				css=0
-				for i in range(3):
-					lss.append([' ']*78)
 				ls=[]
 				for i in range(20):
 					ls.append(([' ']*78))
 				fly=1
 				flys=1
-				if lev > 1:
+				if lev > 1: # if level <= 20
 					lev-=1
-				else:
+				else: # else display the screen with the sword.
 					while 1:
 						window.erase()
 						window.border(0)
@@ -96,7 +83,7 @@ def game():
 						window.addstr(x-1, y-1, '\\', curses.color_pair(1));window.addstr(x-1, y, '|', curses.color_pair(3));window.addstr(x-1, y+1, '/', curses.color_pair(8))
 						window.addstr(x, y-1, '|#|', curses.color_pair(7))
 						window.addstr(21, 40, 'A', curses.color_pair(2))
-						window.addstr(20, 40, '!', curses.color_pair(6))
+						window.addstr(20, 40, '!', curses.color_pair(6)) # the sword
 						q=window.getch()
 						if (x==21 or x==20 or x==19) and (y==39 or y== 40 or y==41):
 							window.erase()
@@ -132,32 +119,32 @@ def game():
 						else:
 							if ord('q') == q:
 								return
-			if q == curses.KEY_RIGHT and y<77 and (window.instr(x,y+2,1)!='#'):
+			if q == curses.KEY_RIGHT and y<77 and (window.instr(x,y+2,1)!='#'): # checking if q is the Right key and there isn't a object in the way.
 				y+=1
-			elif q == curses.KEY_LEFT and y > 2 and (window.instr(x,y-2,1)!='#'):
+			elif q == curses.KEY_LEFT and y > 2 and (window.instr(x,y-2,1)!='#'): # checking if q is the Left key and there isn't a object in the way.
 				y-=1
-			elif q == curses.KEY_UP and fly<2:
+			elif q == curses.KEY_UP and fly<2: # Checking whether the user wants to jump or not.
 				fly=2
-			if fly>1:
-				if flys==1 and c < 5:
+			if fly>1: # if the user wants to jump:
+				if flys==1 and c < 5: #going up
 					x-=1
 					c+=1
 				if c == 5:
 					flys=2
-				if flys==2 and c > 0:
+				if flys==2 and c > 0: # going down
 					x+=1
 					c-=1
-					if (window.instr(x+1,y, 1)=='#' or window.instr(x+1,y-1, 1)=='#' or window.instr(x+1,y+1, 1)=='#') and fly>1 and flys==2:
+					if (window.instr(x+1,y, 1)=='#' or window.instr(x+1,y-1, 1)=='#' or window.instr(x+1,y+1, 1)=='#') and fly>1 and flys==2: # checking wether there is a block under us	
 						fly=1
 						flys=1
 						c=0
 				if c==0:
 					fly=1
 					flys=1
-			elif (window.instr(x+1,y, 1)==' ' and window.instr(x+1,y-1, 1)==' ' and window.instr(x+1,y+1, 1)==' ') and fly<2:
+			elif (window.instr(x+1,y, 1)==' ' and window.instr(x+1,y-1, 1)==' ' and window.instr(x+1,y+1, 1)==' ') and fly<2: #checking in wether we need to go down or not.
 				c=0
 				x+=1
-			if ls[x-2][y-1]=='|' or ls[x-2][y-2]=='|' or ls[x-2][y] == '|':
+			if ls[x-2][y-1]=='|' or ls[x-2][y-2]=='|' or ls[x-2][y] == '|': #checking for bullets that are hitting us
 				if health > 1:
 					health-=1
 					curses.flash()
@@ -170,15 +157,15 @@ def game():
 					q=window.getch()
 					time.sleep(1)
 					return
-			if cs == 4:
+			if cs == 4: # Making new bullets
 				cs=0
 				ls.pop()
 				ls=[([' ']*78)]+ls
 				for i in range(levs[lev]):
 					ls[0][ran(0, 77)]='|'
 			cs+=1
-		else:
-			if ord('q') == q:
+		else: # if paused
+			if ord('q') == q: #if the user want's to quit
 				return
 def menu():
 	dims = window.getmaxyx()
